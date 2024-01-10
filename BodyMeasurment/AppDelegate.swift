118f4bc8bd29a5503f6cf747a04e285a01043704
Ankur kumar <ankur.kumar@iphtechnologies.com>
2024-01-10 11:48:00 +0530

    Initial commit

---
+++ BodyMeasurment/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BodyMeasurment
+//
+//  Created by iPHTech21 on 02/01/24.
+//
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    return true
+  }
+}
